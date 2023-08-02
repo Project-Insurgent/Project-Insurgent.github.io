d44e8c5d2f6cd1f3bc58a8e7514192895ae7aea9
Somersault <66203219+Somersault0023@users.noreply.github.com>
2023-08-02 14:38:58 +0200

    Add files via upload

---
+++ Somersault/js/main.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+  var x = document.getElementById("pwd").value;
+
+  if(x === "PASSWORD"){
+  //document.getElementById("pwdCont").remove();
+    window.location.href = "https://sites.google.com/view/portafoliodocenteb2/inicio";
+  }
+}
