4d487506166ab23f78bd61fcd1fc3eab591eecf7
Somersault0023 <66203219+Somersault0023@users.noreply.github.com>
2023-10-07 20:12:41 +0200

    Header tab-nav

--- somer-hub/src/app/app.component.ts
+++ somer-hub/src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MaterialModule } from "./Material.module";
+//import { BrowserModule } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MaterialModule
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'somer-hub';
+  readonly MIN_WIDTH_THRES: number = 550;
+  public getScreenWidth: number;
+  public getScreenHeight: number;
+  public tabIndex: number;
+
+  constructor() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    this.tabIndex = 0;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+  }
 }
--- somer-hub/src/app/app.routes.ts
+++ somer-hub/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{path: 'home', loadComponent: () => import('./app.component').then(c => c.AppComponent)},
+    //{path: 'scripts', loadComponent: () => import('./components/scripts/scripts-tab/scripts-tab.component').then(c => c.ScriptsTabComponent)},
+    //{path: 'sprites', loadComponent: () => import('./components/sprites/sprites-tab/sprites-tab.component').then(c => c.SpritesTabComponent)},
+    //{path: 'fangames', loadComponent: () => import('./components/fangames/fangames-tab/fangames-tab.component').then(c => c.FangamesTabComponent)},
+    //{path: '', redirectTo: 'home', pathMatch: 'full'}
+];
